925b110b9db4ff4586e374cbe9f90fc24670fb97
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-16 02:45:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts (0...3).collect do |row_index|
+    i = row_index*3
+    row = board[i...i+3]
+    row.join(' | ')
+  end.join('-'*11)
+end
